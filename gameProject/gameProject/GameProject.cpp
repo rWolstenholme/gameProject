f1fc6c5beb144601501293c1a7c87aaec76848a4
Ryan Wolstenholme <ryanwolstenholme@gmail.com>
2013-05-22 15:44:49 +1200

    SDL = working BMP test
    much better

--- gameProject/gameProject/GameProject.cpp
+++ gameProject/gameProject/GameProject.cpp
@@ -1,23 +1,33 @@
- #include "SDL.h"
+#include "SDL.h"
 
 int main(int argc, char *argv[])
 {
 
-SDL_Surface *screen;
+	SDL_Surface *screen;
 
-atexit(SDL_Quit);
+	atexit(SDL_Quit);
 
-if( SDL_Init(SDL_INIT_VIDEO) < 0 ) exit(1);
+	if( SDL_Init(SDL_INIT_VIDEO) < 0 ) exit(1);
 
-SDL_WM_SetCaption("SDL Window", NULL);
+	SDL_WM_SetCaption("SDL Window", NULL);
 
-screen = SDL_SetVideoMode( 640 , 480 , 32 , SDL_DOUBLEBUF|SDL_HWSURFACE|SDL_ANYFORMAT);
+	screen = SDL_SetVideoMode( 640 , 480 , 32 , SDL_DOUBLEBUF|SDL_HWSURFACE|SDL_ANYFORMAT);
 
-SDL_FillRect(screen , NULL , 0x66FF00);
+	SDL_FillRect(screen , NULL , 0xABCDEF);
 
-SDL_Flip(screen);
+	SDL_Surface *image;
+	image = SDL_LoadBMP("ninja.bmp");
+	if (image->format->palette && screen->format->palette) {
+		SDL_SetColors(screen, image->format->palette->colors, 0,
+			image->format->palette->ncolors);
+	}
+	if(SDL_BlitSurface(image, NULL, screen, NULL) < 0)
+		fprintf(stderr, "BlitSurface error: %s\n", SDL_GetError());
+	SDL_UpdateRect(screen, 0, 0, image->w, image->h);
 
-SDL_Delay( 5000 );
+	SDL_Flip(screen);
 
-return 0;
+	SDL_Delay( 5000 );
+
+	return 0;
 }
