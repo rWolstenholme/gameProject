693267effef6d3da12ed1da8fbfa5f1008bb008a
Garming <garming_sam@outlook.com>
2013-05-23 20:32:50 +1200

    Added icon, tested PNG

--- gameProject/gameProject/GameProject.cpp
+++ gameProject/gameProject/GameProject.cpp
@@ -1,4 +1,5 @@
 #include "SDL.h"
+#include "SDL_image.h"
 
 int main(int argc, char *argv[])
 {
@@ -11,12 +12,18 @@ int main(int argc, char *argv[])
 
 	SDL_WM_SetCaption("SDL Window", NULL);
 
+	//Set icon file here
+	SDL_Surface *icon = IMG_Load("Resources/favicon.ico");
+	SDL_WM_SetIcon(icon, NULL);
+
 	screen = SDL_SetVideoMode( 640 , 480 , 32 , SDL_DOUBLEBUF|SDL_HWSURFACE|SDL_ANYFORMAT);
 
 	SDL_FillRect(screen , NULL , 0xABCDEF);
 
 	SDL_Surface *image;
-	image = SDL_LoadBMP("ninja.bmp");
+	//image = SDL_LoadBMP("ninja.bmp");
+	image = IMG_Load("Resources/Sprites/grassDirt.png");
+	//SDL_SetAlpha(image, SDL_SRCALPHA, 255); no idea what this does
 	if (image->format->palette && screen->format->palette) {
 		SDL_SetColors(screen, image->format->palette->colors, 0,
 			image->format->palette->ncolors);
